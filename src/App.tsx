713d6f16579d1df63855934d9731398ebecc73ab
s4kshidevx <sakshisingh5x19@gmail.com>
2025-12-26 01:33:56 +0530

    fin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,101 @@
-import {useState} from 'react'
-import words from './wordList.json'
-import { HangmanDrawing } from './HangmanDrawing'
-import { HangmanWord } from './HangmanWord'
-import {Keyboard} from './Keyboard'
+import { useCallback, useEffect, useState } from "react"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
+import words from "./wordList.json"
+
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  const [wordToGuess, setWordToGuess] = useState(()=>{
-   
-    return words [Math.floor(Math.random()* words.length)]
-    
-  })
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  )
 
-  const[guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = wordToGuess
+    .split("")
+    .every(letter => guessedLetters.includes(letter))
 
-  const inCorrectLetters= guessedLetters.filter(letter => !wordToGuess.includes(letter))
-  
-  
-       
-     return <div
-     style={{
-      maxWidth: '800px',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '2rem',
-      margin: '0 auto',
-      alignItems: 'center'
-     }}>
-      <div style={{fontSize: '2rem',
-        textAlign: 'center'
-      }}>
-       Lose, Win
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    },
+    [guessedLetters, isWinner, isLoser]
+  )
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (key !== "Enter") return
+
+      e.preventDefault()
+      setGuessedLetters([])
+      setWordToGuess(getWord())
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && "Winner! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
       </div>
-
-      <HangmanDrawing numberOfGuesses={inCorrectLetters.length} />
-      <HangmanWord />
-      <div style={{ alignSelf: 'stretch'}}>
-      <Keyboard />
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
       </div>
-      
-      </div>
-   
-  
+    </div>
+  )
 }
 
 export default App
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,4 @@
-import styles from './Keyboard.module.css'
+import styles from "./Keyboard.module.css"
 
 const KEYS = [
   "a",
@@ -29,25 +29,43 @@ const KEYS = [
   "z",
 ]
 
-export function Keyboard(){
-    return(
-    <div style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
-         gap: '.5rem'
-    }}>
+type KeyboardProps = {
+  disabled?: boolean
+  activeLetters: string[]
+  inactiveLetters: string[]
+  addGuessedLetter: (letter: string) => void
+}
 
-    {KEYS.map(key =>{
-      return(
-        <button
-        className={`${styles.btn} `} key={key}>
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
             {key}
-        </button>
-      )  
-    })}
-        
-
-       
+          </button>
+        )
+      })}
     </div>
-    )
+  )
 }
